d4e9d98b1727feb0940a4e8f2c41489596c065e5
PEETCH1E <160307750+PEETCH1E@users.noreply.github.com>
2024-03-12 16:42:33 +0700

    SDDDDSADASDSAD

--- autiqueApp/models.py
+++ autiqueApp/models.py
@@ -1,7 +1,33 @@
+#รับซื้อของเก่า
 #พนักงาน Employee
+    # --ID :: รหัสบัตรปปช
+    # --Username :: ชื่อผู้ใช้
+    # --Email :: Email
+    # --firstName :: ชื่อจริง
+    # --lastName :: นามสกุล
+    # --Address :: ที่อยู่
+    # --Tell :: เบอร์โทร่
+    # --Password :: รหัสผ่าน (Save as Auth user)
+    # Admin Added
 #ลูกค้า Customer
-
+    # --ID :: รหัสบัตรปปช Auto
+    # --firstName :: ชื่อจริง
+    # --lastName :: นามสกุล
+    # --Address :: ที่อยู่
+    # --Tell :: เบอร์โทร่
 #ประเภทของ type
-#คลังของเก่า Inventory
+    # --ID :: รหัสสินค้า
+    # --PricePerUnit :: ราคาสินค้าต่อน้ำหนัก
+    # --totalPrice :: ราคาสินค้ารวม
+    # --Amount :: จำนวน
+    # --DateAdded :: วันที่เพิ่มสินค้า
 #รายการรับซื้อ PurcheseOrder
-#รายการรับขาย SalesOrder
+    # --ID :: ไอดีรายการรับซื้อ
+    # --InventoryID :: ??3
+    # --Amount :: จำนวนที่ซื้อ
+    # --PricePerUnit :: ราคาต่อหน่วย
+    # --PriceAll :: ราคารวม
+    # --Date :: วันที่รับซื้อ (ไม่ต้องกรอกมั้ย) **รอพิจารณา
+    # --cus_id :: ไอดีลูกค้า (FK) **รอพิจารณาอีกที
+#รายการขาย SalesOrder
+    # --ID :: ไอดีรายการขาย
